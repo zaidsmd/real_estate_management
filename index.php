37f7f7f801ab0452ee552e29a8825627e8774eec
Zaid Samadi <zaidsmd111@gmail.com>
2023-02-03 11:41:45 +0100

    done edit

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ include "dbconfig.php"
                     <label for="range_max">Max:</label>
                     <input type="number" name="max" id="range_max" value="3000000000">
                 </div>
-                <input type="text" name="check" style="display: none">
+                <input type="text" name="check" value="true" style="display: none">
                 <button type="submit" class="btn btn-primary" id="search"><i class="fa-solid fa-magnifying-glass"></i>
                 </button>
             </form>
@@ -59,23 +59,17 @@ include "dbconfig.php"
         <div class="cards d-flex flex-wrap">
             <?php
             if (isset($_GET["check"])) {
+                $max = $_GET["max"];
+                $min = $_GET["min"];
+                $type = $_GET["type"];
                 if (isset($_GET["max"]) && isset($_GET["min"]) && $_GET["type"] != 0) {
-                    $max = $_GET["max"];
-                    $min = $_GET["min"];
-                    $type = $_GET["type"];
                     $statement = $conn->prepare("SELECT * FROM `annonces` WHERE annonce_type = '$type' AND annonce_price BETWEEN $min AND $max");
-                    $statement->execute();
-                    $result = $statement->fetchAll();
-                    createCard($result);
                 } else {
-                    $max = $_GET["max"];
-                    $min = $_GET["min"];
-                    $type = $_GET["type"];
                     $statement = $conn->prepare("SELECT * FROM `annonces` WHERE annonce_price BETWEEN $min AND $max");
-                    $statement->execute();
-                    $result = $statement->fetchAll();
-                    createCard($result);
                 }
+                $statement->execute();
+                $result = $statement->fetchAll();
+                createCard($result);
             } else {
                 $statement = $conn->prepare("SELECT * FROM `annonces`");
                 $statement->execute();
@@ -116,7 +110,7 @@ include "dbconfig.php"
                         <p class="price">$475,000.00</p>
                         <div class="buttons">
                             <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#del-modal"  type="button" id="delete">Delete</button>
-                            <button class="btn btn-primary" id="edit">Edit</button>
+                            <button class="btn btn-primary" id="edit" data-bs-toggle="modal" data-bs-target="#modify-modal">Edit</button>
                         </div>
                     </div>
                 </div>
@@ -144,12 +138,48 @@ include "dbconfig.php"
                     <button type="button" id="close" class="btn-close" data-bs-dismiss="modal"
                             aria-label="Close"></button>
                     <div class="modal-body">
-                        <h4 class="title"></h4>
-                        <form action="delete.php" method="post">
-                            <input type="text" name="annonce_id" id="annonce_id" style="display: none">
-                            <input type="text">
-                            <button data-bs-dismiss="modal" type="button" class="btn btn-primary">Cancel</button>
-                            <input type="submit" name="modify" id="Modify" class="btn btn-primary" value="Modify">
+                        <h4 class="title">Edit the annonce</h4>
+                        <p>Please fill every input all inputs are required.</p>
+                        <form action="edit.php" method="post" enctype="multipart/form-data">
+                            <div class="input-group">
+                                <div class="input">
+                                    <label for="title_modify">Annonce title</label>
+                                    <input type="text" id="title_modify" maxlength="40" name="title">
+                                </div>
+                                <div class="input">
+                                    <label for="adresse_modify">Annonce adresse</label>
+                                    <input type="text" maxlength="50" id="adresse_modify" name="adresse">
+                                </div>
+                            </div>
+                            <div class="input-group">
+                                <div class="input">
+                                    <label for="area_modify">Annonce area(m²)</label>
+                                    <input type="text" maxlength="11" id="area_modify" name="area">
+                                </div>
+                                <div class="input">
+                                    <label for="price_modify">Annonce price</label>
+                                    <input type="text" maxlength="11" id="price_modify" name="price">
+                                </div>
+                            </div>
+                            <div class="input-group">
+                                <div class="input">
+                                    <label for="type_modify">Annonce type</label>
+                                    <select name="type" id="type_modify">
+                                        <option value="For Sale">For Sale</option>
+                                        <option value="For Rent">For Rent</option>
+                                    </select>
+                                </div>
+                                <div class="input">
+                                    <label for="image_modify">Annonce image (jpg-png-jpeg)</label>
+                                    <input type="file" id="image_modify" name="image">
+                                </div>
+                            </div>
+                            <div class="input">
+                                <label for="description_modify">Annonce description</label>
+                                <textarea name="description"  maxlength="800" id="description_modify" rows="5"></textarea>
+                            </div>
+                            <input type="text" name="annonce_id" id="modify_id">
+                            <input class="btn btn-primary" type="submit" value="Edit">
                         </form>
                     </div>
                 </div>
@@ -207,13 +237,13 @@ include "dbconfig.php"
                     </div>
                 </div>
             </div>
-        </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
             crossorigin="anonymous"></script>
     <script>
         function show(data) {
+            let dataArray = {"annonce_id":data.annonce_id};
             document.querySelector('.modal-img img').src = "pictures/"+data["annonce_image"];
             document.querySelector('#modal .title').innerHTML = data["annonce_title"];
             document.querySelector('#modal .tag:first-child').innerHTML = data["annonce_type"];
@@ -228,9 +258,22 @@ include "dbconfig.php"
             }
             document.querySelector('#modal #delete').dataset.id = data["annonce_id"];
             document.querySelector('#modal #delete').setAttribute('onclick','deleteModal(this.dataset.id)');
+            document.querySelector('#modal #edit').dataset.id = data["annonce_id"];
+            document.querySelector('#modal #edit').setAttribute('onclick','modify_modal('+JSON.stringify(data)+')');
         }
         function deleteModal(id){
             document.querySelector('#del-modal #annonce_id').value = id;
+        }
+        function modify_modal(data){
+            document.querySelector('#modify-modal #modify_id').value = data["annonce_id"];
+            document.querySelector('#modify-modal #adresse_modify').value= data["annonce_adresse"];
+            document.querySelector('#modify-modal #area_modify').value= data["annonce_area"];
+            document.querySelector('#modify-modal #price_modify').value= +data["annonce_price"];
+            document.querySelector('#modify-modal #description_modify').value= data["annonce_description"];
+            document.querySelector('#modify-modal #title_modify').value= data["annonce_title"];
+            if (data["annonce_type"] == "For Rent") {
+                document.querySelector('#modify-modal option[value="For Rent"]').setAttribute('selected','');
+            }
         }
         <?php
         if (isset($_GET["check"])) {
@@ -250,7 +293,7 @@ function createCard($data)
     foreach ($data as $row) {
         echo '
             <div class="card-container col-3">
-           <div class="card"data-id="' . $row["annonce_id"] . '"onclick=\'show(' . json_encode($row) . ')\' data-bs-toggle="modal" data-bs-target="#modal">
+           <div class="card" data-id="' . $row["annonce_id"] . '"onclick=\'show(' . json_encode($row) . ')\' data-bs-toggle="modal" data-bs-target="#modal">
                <img src="pictures/' . $row["annonce_image"] . '" class="card-img-top" alt="' . $row["annonce_title"] . '">
                <div class="card-body">
                    <h4 class="card-title">' . $row["annonce_title"] . '</h4>
@@ -260,7 +303,7 @@ function createCard($data)
                        <div class="tag">' . $row["annonce_date"] . '</div>
                    </div>
                    <div class="adresse">' . $row["annonce_adresse"] . '</div>
-                   <p class="price" >$' . number_format($row["annonce_price"], 2, '.', ',') . '</p>
+                   <p class="price" >$' . number_format($row["annonce_price"], 2) . '</p>
                </div>
            </div>
        </div>';
